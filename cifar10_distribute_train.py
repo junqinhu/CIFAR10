439743a6c0949e039af2619263afb9b2882adc11
jiuweilinghu <1354895960@qq.com>
2019-02-26 17:37:49 +0800

    又出现会话一直等待响应的问题
    CreateSession still waiting for response from worker: /job:ps/replica:0/task0

--- cifar10_distribute_train.py
+++ cifar10_distribute_train.py
@@ -3,9 +3,9 @@ A binary to train CIFAR-10 using multi_GPU in some devices.
 Here we use 3 terminals to represent 2 works devices and 1 ps device
 This code can only run on the terminal
 Please use the following command to run the code:
-    terminal 1： python cifar10_distribute_Train.py --job_name=ps --task_index=0
-    terminal 2： python cifar10_distribute_Train.py --job_name=worker --task_index=1
-    terminal 3： python cifar10_distribute_Train.py --job_name=worker --task_index=0
+    terminal 1： python cifar10_distribute_train.py --job_name=ps --task_index=0
+    terminal 2： python cifar10_distribute_train.py --job_name=worker --task_index=1
+    terminal 3： python cifar10_distribute_train.py --job_name=worker --task_index=0
 """
 from __future__ import absolute_import
 from __future__ import division
@@ -86,7 +86,7 @@ def train():
 
     # 获取同步更新模型参数所需要的副本数
     num_workers = len(worker_spec)
-    print("worker numbers:%d"%num_workers)
+    print("worker numbers:%d" % num_workers)
     # 创建集群
     cluster = tf.train.ClusterSpec({'ps': ps_spc,
                                     'worker': worker_spec})
@@ -98,30 +98,23 @@ def train():
         server.join()
 
     for i in range(FLAGS.loop_steps):
+        print('i:%d' % i)
         # 从配置文件中读取配置内容，并以字典形式存储
         # 配置信息为每个节点每批次训练的数据量
         # 格式为{‘workerX：batch_size’}
         # 如果当前节点是parameter server，则不再进行后续的操作，
         # 而是使用server.join等待worker工作
         f = open(FLAGS.config_dir)
-        dic_config = eval(f.read())
-        FLAGS.replicas_to_aggregate = len(dic_config)
+        dic_config = eval(f.readline())
+        print(dic_config)
         dic_key = FLAGS.job_name + str(FLAGS.task_index)
-        if (dic_key in dic_config):
+        if dic_key in dic_config:
             FLAGS.batch_size = dic_config[dic_key]
         # 如果batch_size为0，则此节点不工作
-        if (FLAGS.batch_size == 0):
+        if FLAGS.batch_size == 0:
             server.join()
 
         is_chief = (FLAGS.task_index == 0)
-
-        if FLAGS.num_gpus > 0:
-            # 避免gpu分配冲突：现在为相应机器中的每个worker分配task_num - > #gpu
-            gpu = (FLAGS.task_index % FLAGS.num_gpus)
-            worker_device = "/job:worker/task:%d/gpu:%d" % (FLAGS.task_index, gpu)
-        elif FLAGS.num_gpus == 0:
-            cpu = 0
-            worker_device = "/job:worker/task:%d/cpu:%d" % (FLAGS.task_index, cpu)
 
         # 我们使用 tf.train.replica_device_setter 将涉及变量的操作分配到参数服务器上，并使用 CPU;
         # 将涉及非变量的操作分配到工作节点上，使用 worker_device 的值。
@@ -130,13 +123,13 @@ def train():
         # 在深度学习训练中，一般图的计算，对于每个worker task来说，都是相同的，
         # 所以我们会把所有图计算、变量定义等代码，都写到下面这个语句下。
         device_setter = tf.train.replica_device_setter(
-            worker_device=worker_device,
+            worker_device="/job:worker/task:%d/cpu:0" % (FLAGS.task_index),
             ps_device='/job:ps/cpu:0',
             cluster=cluster)
 
         with tf.device(device_setter):
             global_step = tf.Variable(FLAGS.global_step, trainable=False, name='global_step')
-            if(i>0):
+            if i > 0:
                 tf.get_variable_scope().reuse_variables()
 
             # Get images and labels for CIFAR-10.
@@ -158,27 +151,10 @@ def train():
                                             decay_steps,
                                             LEARNING_RATE_DECAY_FACTOR,
                                             staircase=True)
-            loss_averages = tf.train.ExponentialMovingAverage(0.9, global_step)
-            losses = tf.get_collection('losses')
-            variables_to_average=losses + [loss]
-            # 创建同步训练的优化器
-            # tf.train.SyncReplicasOptimizer实质上是对原有优化器的一个扩展，
-            # 我们传入原有优化器及其他参数，
-            # 它会将原有优化器改造为同步分布式训练版本
-            opt = tf.train.SyncReplicasOptimizer(
-                tf.train.GradientDescentOptimizer(lr),
-                replicas_to_aggregate=FLAGS.replicas_to_aggregate,
-                total_num_replicas=num_workers)
+            # 创建优化器
+            opt=tf.train.GradientDescentOptimizer(lr)
             # 记得传入global_step以同步
             train_op = opt.minimize(loss, global_step=global_step)
-            scaffold=tf.train.Scaffold(ready_op=global_step)
-            # 如果是chief task
-            #if is_chief:
-            #    loss_averages = tf.train.ExponentialMovingAverage(0.9, global_step)
-            #    losses = tf.get_collection('losses')
-            #    loss_averages_op = loss_averages.apply(losses + [loss])
-            #    with tf.control_dependencies([loss_averages_op]):
-            #        train_op = loss_averages_op
 
             class _LoggerHook(tf.train.SessionRunHook):
                 """Logs loss and runtime."""
@@ -206,18 +182,11 @@ def train():
                         print(format_str % (datetime.now(), self._step, loss_value,
                                             examples_per_sec, sec_per_batch))
 
-            hooks = [opt.make_session_run_hook(is_chief),
-                     tf.train.StopAtStepHook(num_steps=FLAGS.num_steps),
+            hooks = [tf.train.StopAtStepHook(num_steps=FLAGS.num_steps),
                      tf.train.NanTensorHook(loss),
                      _LoggerHook()]
 
             sess_config = tf.ConfigProto(
-                # 软放置
-                # 如果该操作函数没有 GPU 实现时
-                # 会自动使用 CPU 设备
-                allow_soft_placement=True,
-                # 是否打印操作所对应的执行设备的信息
-                log_device_placement=False,
                 # device_filters:硬件过滤器
                 # 如果被设置的话，会话会忽略掉所有不匹配过滤器的硬件。
                 device_filters=["/job:ps",
@@ -226,7 +195,6 @@ def train():
                     master=server.target,
                     is_chief=is_chief,
                     checkpoint_dir=FLAGS.train_dir,
-                   # scaffold=scaffold,
                     hooks=hooks,
                     config=sess_config) as mon_sess:
                 while not mon_sess.should_stop():
